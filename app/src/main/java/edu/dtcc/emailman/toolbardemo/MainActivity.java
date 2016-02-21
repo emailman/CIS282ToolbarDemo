d182b12621a2726439697825c678ec7c0783d837
emailman <emailman@dtcc.edu>
2016-02-21 11:25:37 -0500

    Added several actions to the toolbar

--- app/src/main/java/edu/dtcc/emailman/toolbardemo/MainActivity.java
+++ app/src/main/java/edu/dtcc/emailman/toolbardemo/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,13 +42,32 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action1:
+                Snackbar.make(findViewById(R.id.myCoordinatorLayout),
+                        "This is Action 1.\tWatch the floating action button move out off the way.",
+                        Snackbar.LENGTH_LONG).show();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action2:
+                Snackbar.make(findViewById(R.id.myCoordinatorLayout),
+                        "This is Action 2.  You can right swipe me out off the way.",
+                        Snackbar.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action3:
+                Toast toast= Toast.makeText(getApplicationContext(),
+                        "Toast messages are toast, use Snackbar instead",
+                        Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                return true;
+
+            case R.id.action_settings:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
